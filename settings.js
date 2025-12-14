8b31eaa8627ea6f29c987871ec9ee6cca1d54e70
favourite111 <eminentboy12345@gmail.com>
2025-12-14 11:34:32 +0100

    Initial commit

---
+++ settings.js
@@ -0,0 +1,24 @@
+const settings = {
+ 
+  botName: process.env.BOT_NAME || "",
+ 
+  ownerNumber: process.env.OWNER_NUMBER || '',
+
+ botOwner: process.env.BOT_OWNER || '',
+ 
+  packname: process.env.PACKNAME || '',
+ 
+  prefix: process.env.PREFIX || '',
+ 
+  defaultReaction: '✅',
+ 
+  errorReaction: '❌',
+ 
+  watermark: "©Gift md is on fire!🔥",
+ 
+  description: "This is a bot for managing group commands and automating tasks.",
+ 
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', 
+};
+
+export default settings;
